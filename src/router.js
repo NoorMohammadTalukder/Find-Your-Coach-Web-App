6f9e75267cc3f8275b5703c9f81fa9e7ea423295
YourName <noor11809027@gmail.com>
2022-09-30 18:57:55 +0600

    component add

--- src/router.js
+++ src/router.js
@@ -1,23 +1,32 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 
+import CoachDeatail from './pages/coaches/CoachDeatail.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestReceived from './pages/requests/RequestReceived.vue';
+
+import NotFound from './pages/NotFound.vue';
+
 const router = createRouter({
   history: createWebHashHistory(),
   routes: [
     { path: '/', redirect:'/coaches' },
 
-    { path: '/coaches', component: null },
+    { path: '/coaches', component: CoachesList },
 
     {
       path: '/coaches/:id',
-      component: null,
-      children: [{ path: '/contact', component: null }],// coaches/c1/contact
+      component: CoachDeatail,
+      children: [{ path: '/contact', component: ContactCoach }],// coaches/c1/contact
     },
 
-    { path: '/register', component: null },
+    { path: '/register', component: CoachRegistration },
 
-    { path: '/requests', component: null },
-    
-    { path: '/:notfound(.*)', component: null },
+    { path: '/requests', component: RequestReceived },
+
+    { path: '/:notfound(.*)', component: NotFound },
   ],
 });
 
