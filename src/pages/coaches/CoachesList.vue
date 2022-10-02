5256a5c243a6435ec8a0706ddd6e8de4393815d9
YourName <noor11809027@gmail.com>
2022-10-02 13:27:28 +0600

    git commit -m hasCoaches

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -8,5 +8,27 @@
       <router-link to="/register">Register as Coach</router-link>
 
     </div>
+
+    <ul v-if="hasCoaches">
+      <li v-for="coach in filteredCoaches" :key="coach.id">
+          {{coach.firstName}}
+      </li>
+    </ul>
+    <h3 v-else>No Coaches Found...</h3>
   </section>
 </template>
+
+<script>
+export default {
+  computed:{
+    filteredCoaches(){
+      // console.log(this.$store.getters["coaches/coaches"]);
+      return this.$store.getters['coaches/coaches']; 
+    },
+
+    hasCoaches(){
+      return this.$store.getters['coaches/hasCoaches']; 
+    }
+  }
+}
+</script>
