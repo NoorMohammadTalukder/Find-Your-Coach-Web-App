78036afc0e392700d6258c667e8894edeee4d1c0
YourName <noor11809027@gmail.com>
2022-10-02 13:58:03 +0600

    coach list add

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -3,32 +3,54 @@
 
   <section>
     <div class="controls">
-
       <button>Refresh</button>
       <router-link to="/register">Register as Coach</router-link>
-
     </div>
 
     <ul v-if="hasCoaches">
-      <li v-for="coach in filteredCoaches" :key="coach.id">
-          {{coach.firstName}}
-      </li>
+      <!-- <li v-for="coach in filteredCoaches" :key="coach.id">
+        {{ coach.firstName }}
+      </li> -->
+      <coach-item
+        v-for="coach in filteredCoaches"
+        :key="coach.id"
+        :id="coach.id"
+        :first-name="coach.firstName"
+        :last-name="coach.lastName"
+        :rate="coach.hourlyRate"
+        :areas="coach.areas"
+      ></coach-item>
     </ul>
     <h3 v-else>No Coaches Found...</h3>
   </section>
 </template>
 
 <script>
+  import CoachItem from '../../components/coaches/CoachItem.vue'
 export default {
-  computed:{
-    filteredCoaches(){
+  components:{CoachItem},
+  computed: {
+    filteredCoaches() {
       // console.log(this.$store.getters["coaches/coaches"]);
-      return this.$store.getters['coaches/coaches']; 
+      return this.$store.getters['coaches/coaches'];
     },
 
-    hasCoaches(){
-      return this.$store.getters['coaches/hasCoaches']; 
-    }
-  }
-}
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    },
+  },
+};
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
